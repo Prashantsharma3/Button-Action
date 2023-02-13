2b06d0de2b59d93e9661f20cf7f00113b0f4884f
Prashant Sharma <prashantsharmaxxx1@gmail.com>
2023-02-13 21:24:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+function MyButton() {
+  // Declare a state variable called "showText" and initialize it to false
+  const [showText, setShowText] = useState(false);
 
-function App() {
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div>
+      <button id="click" onClick={() => setShowText(!showText)}>
+        Click Me
+      </button>
+      {showText && (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      )}
     </div>
   );
 }
 
+export default MyButton;
 
-export default App;
