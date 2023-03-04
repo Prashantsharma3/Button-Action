315c685dc8eb7c4f68450ac3f29c905d4843e2ed
Prashant Sharma <116828172+Prashantsharma3@users.noreply.github.com>
2023-03-04 21:14:27 +0530

    Newton

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,18 @@
-import React, {Component, useState} from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
-const Assignment = () => {
-  // Initialize the state with false, meaning the paragraph is not visible
+
+const App = () => {
   const [isVisible, setIsVisible] = useState(false);
 
-  // On button click, toggle the state of isVisible
   const handleClick = () => {
     setIsVisible(!isVisible);
   };
 
   return (
     <div>
-      {/* Render button with id "click" */}
       <button id="click" onClick={handleClick}>
         Click Me
       </button>
-      {/* Conditionally render the paragraph based on isVisible state */}
       {isVisible && (
         <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
@@ -25,4 +22,5 @@ const Assignment = () => {
   );
 };
 
-export default Assignment;
+export default App;
+
